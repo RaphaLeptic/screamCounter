47948bfcfcf9c261e453202f3aecb377ef95fd01
RaphaLeptic <82365801+RaphaLeptic@users.noreply.github.com>
2021-04-13 14:55:40 +0200

    added scream detector

--- main.ts
+++ main.ts
@@ -1,28 +1,30 @@
-input.onButtonPressed(Button.A, function () {
-    number += 1
-    display.show(number)
-})
-let soundLevel = 0
+let maxSound = 0
 let number = 0
-let display: grove.TM1637 = null
-display = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
+let soundLevel = 0
+let display = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
+let display2 = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 basic.forever(function () {
     soundLevel = input.soundLevel()
-    if (soundLevel > 75) {
+    if (soundLevel < 50) {
+        number += 0
+        display.show(number)
+    } else if (soundLevel > 50) {
         number += 1
         display.show(number)
-    } else if (soundLevel > 125) {
-        number += 2
-        display.show(number)
-    } else if (soundLevel > 175) {
+    } else if (soundLevel > 100) {
         number += 3
         display.show(number)
-    } else if (soundLevel > 225) {
+    } else if (soundLevel > 200) {
         number += 5
         display.show(number)
     } else {
-        soundLevel += 8
+        number += 8
         display.show(number)
     }
     basic.pause(100)
+    maxSound = 256
+    soundLevel = soundLevel - 50
+    soundLevel = soundLevel * 5
+    soundLevel = Math.constrain(soundLevel, 0, maxSound)
+    display2.showBarGraph(soundLevel, maxSound)
 })
